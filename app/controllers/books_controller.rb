1e370a3c6e4cc0fd521ee592f61650aaf86ddbad
EC2 Default User <ec2-user@ip-172-31-14-130.ap-northeast-1.compute.internal>
2023-11-19 11:06:12 +0000

    不必要なコントローラーを消しました。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,9 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-  end
-
   def create
     @book = Book.new(book_params)
-    
+
     if @book.save
       redirect_to book_path(@book.id), notice:'Book was successfully created.'
     else
@@ -35,7 +32,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    
+
     if @book.update(book_params)
       redirect_to book_path(@book.id), notice:'Book was successfully update.'
     else
